08b63f5af603b96e4d59ea6fef2af89c946c4b12
yjg1 <zxcvc9999@gmail.com>
2022-11-08 17:14:33 +0900

    chore: change jib img path

--- src/main/java/noctem/eurekaServer/EurekaServerApplication.java
+++ src/main/java/noctem/eurekaServer/EurekaServerApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+// 젠킨스 테스트
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
